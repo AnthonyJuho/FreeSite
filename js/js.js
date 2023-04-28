c85a34ea60cbf8ac346efa7f0e7edc0d725f768b
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2023-04-28 16:15:09 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+var pg = document.getElementById("playground");
+var circle = document.getElementById("circle");
+var pgWidth = pg.offsetWidth;
+var pgHeight = pg.offsetHeight;
+var circleWidth = circle.offsetWidth;
+var circleHeight = circle.offsetHeight;
+// circle.style.top;
